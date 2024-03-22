c4452e6e4e1f7075d5c0618cf01cd1c2cdd4e62b
Kaiwen Zhang <2695415649@qq.com>
2024-03-23 00:42:01 +0800

    Update 取模类.cpp

--- 杂/手写/取模类.cpp
+++ 杂/手写/取模类.cpp
@@ -21,31 +21,31 @@ struct mod_int {
         if (x < 0) x += P;
         return x;
     }
-    constexpr void setMod(int x) {
+    constexpr static void setMod(int x) {
         mod = x;
     }
-    constexpr int getMod() {
+    constexpr static int getMod() {
         return (P > 0 ? P : mod);
     }
     constexpr mod_int operator-() {
         return -x;
     }
-    constexpr mod_int &operator+= (mod_int rhs) {
+    constexpr mod_int& operator+= (mod_int rhs) {
         x = norm(x + rhs.x);
         return *this;
     }
-    constexpr mod_int &operator-= (mod_int rhs) {
+    constexpr mod_int& operator-= (mod_int rhs) {
         x = norm(x - rhs.x);
         return *this;
     }
-    constexpr mod_int &operator*= (mod_int rhs) {
+    constexpr mod_int& operator*= (mod_int rhs) {
         x = 1ll * x * rhs.x % getMod();
         return *this;
     }
     constexpr mod_int inv () {
         return power(*this, P - 2);
     }
-    constexpr mod_int &operator/= (mod_int rhs) {
+    constexpr mod_int& operator/= (mod_int rhs) {
         x = 1ll * x * rhs.inv().x % getMod();
         return *this;
     }
@@ -67,13 +67,13 @@ struct mod_int {
     constexpr friend bool operator!= (mod_int lhs, mod_int rhs) {
         return lhs.x != rhs.x;
     }
-    constexpr friend istream &operator>> (istream& flow, mod_int& rhs) {
+    constexpr friend istream& operator>> (istream& flow, mod_int& rhs) {
         long long x;
         flow >> x;
         rhs = x;
         return flow;
     }
-    constexpr friend ostream &operator<< (ostream& flow, mod_int rhs) {
+    constexpr friend ostream& operator<< (ostream& flow, mod_int rhs) {
         return flow << rhs.x;
     }
 };
@@ -81,5 +81,5 @@ struct mod_int {
 template<int P, int x>
 constexpr mod_int<P> invx = mod_int<P>(x).inv();
 
-constexpr int P = 998244353;
+constexpr int P = 1e9 + 7;
 using Z = mod_int<P>;
