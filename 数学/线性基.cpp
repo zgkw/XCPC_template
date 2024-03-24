ce562e9e6621ab544d24e2c9fff73bed632e2c29
Wqsing-win <209954675@qq.com>
2024-03-24 16:38:52 +0800

    Update 线性基.cpp

--- 数学/线性基.cpp
+++ 数学/线性基.cpp
@@ -1,40 +1,43 @@
-template <int Lim>
-struct Linear_Base{
-  int a[Lim + 1] , n ;
-  Linear_Base() {
-    n = Lim ;
-    memset(a,0,sizeof a);
-  }
-  void insert(int x){//插入
-    for(int i=Lim;i>=0;i--)if(x&(1ll<<i)){
-      if(!a[i]){a[i]=x;return;}
-      else x^=a[i];
-  }
+struct Linear_Base {
+    int siz;
+    vector<int> a;  
+    Linear_Base(int _siz) {
+        siz = _siz;
+        a.resize(siz + 1);
     }
-  bool check(int x){//查询x是否能被异或出来
-    for(int i=Lim;i>=0;i--)if(x&(1ll<<i)){
-      if(!a[i])break;
-      x^=a[i];
+    void insert(int x) {//插入
+        for (int i = siz; i >= 0; i--) if (x & (1ll << i)) {
+            if (!a[i]) { a[i] = x; return; }
+            else x ^= a[i];
+        }
     }
-    return x==0;
-  }
-  int querymax(int res){//查询最大异或和
-    for(int i=Lim;i>=0;i--)if((res^a[i])>res)res^=a[i];
-    return res;
-  }
-  int querymin(int res){//查询最小
-    for(int i=Lim;i>=0;i--)if(res&(1ll<<i))res^=a[i];
-    return res;
-  }
-  int querykth(int k){//查询第k大的异或和
-    int tmp[Lim+10],res=0,cnt=0;
-    for(int i=0;i<=Lim;i++){
-      for(int j=i-1;j>=0;j--)if(a[i]&(1ll<<j))a[i]^=a[j];
-      if(a[i])tmp[cnt++]=a[i];
+    bool check(int x) {//查询x是否能被异或出来
+        for (int i = siz; i >= 0; i--) if (x & (1ll << i)) {
+            if (!a[i]) break;
+            x ^= a[i];
+        }
+        return x == 0;
     }
-    for(int i=0;i<cnt;i++)if(k&(1ll<<i))res^=tmp[i];
-    return res;
-  }
-  void merge(const Linear_Base &other)//合并
-  {for(int i=0;i<=Lim;i++) insert(other.a[i]);}
+    int querymax(int res) {//查询最大异或和
+        for (int i = siz; i >= 0; i--) if ((res ^ a[i]) > res) res ^= a[i];
+        return res;
+    }
+    int querymin(int res) {//查询最小
+        for (int i = siz; i >= 0; i--) if (res & (1ll << i)) res ^= a[i];
+        return res;
+    }
+    int querykth(int k) {//查询第k大的异或和
+        vector<int> tmp(siz + 10);
+        int res = 0, cnt = 0;
+        for (int i = 0; i <= siz; i++) {
+            for (int j = i - 1; j >= 0; j--) if (a[i] & (1ll << j)) a[i] ^= a[j];
+            if(a[i]) tmp[cnt++] = a[i];
+        }
+        for (int i = 0; i < cnt; i++) if (k & (1ll << i)) res ^= tmp[i];
+        return res;
+    }
+    void merge(const Linear_Base& other)//合并
+    {
+        for (int i = 0; i <= siz; i++) insert(other.a[i]);
+    }
 };
