015cf0459ae837437f9825674cc6998ff62de7ea
Kaiwen Zhang <2695415649@qq.com>
2024-03-27 12:31:16 +0800

    兼容性、排版优化

--- 数学/组合数学.cpp
+++ 数学/组合数学.cpp
@@ -1,22 +1,23 @@
-template < class T >
+template<class T>
 struct Comb {
     int n;
-    std::vector<T> _fac;
-    std::vector<T> _invfac;
-    std::vector<T> _inv;
-    
+    std::vector <T> _fac;
+    std::vector <T> _invfac;
+    std::vector <T> _inv;
+
     Comb() : n{0}, _fac{1}, _invfac{1}, _inv{0} {}
+
     Comb(int n) : Comb() {
         init(n);
     }
-    
+
     void init(int m) {
         m = std::min(m, T::getMod() - 1);
         if (m <= n) return;
         _fac.resize(m + 1);
         _invfac.resize(m + 1);
         _inv.resize(m + 1);
-        
+
         for (int i = n + 1; i <= m; i++) {
             _fac[i] = _fac[i - 1] * i;
         }
@@ -27,35 +28,43 @@ struct Comb {
         }
         n = m;
     }
-    
+
     T fac(int m) {
         if (m > n) init(2 * m);
         return _fac[m];
     }
+
     T invfac(int m) {
         if (m > n) init(2 * m);
         return _invfac[m];
     }
+
     T inv(int m) {
         if (m > n) init(2 * m);
         return _inv[m];
     }
+
     T binom(int n, int m) {
         if (n < m || m < 0) return 0;
         return fac(n) * invfac(m) * invfac(n - m);
     }
+
 /**
+ * 第二类斯特林数
  * 时间复杂度 : O (m * log (m))
 */
-    T Stirling (int n, int m) {
-        T ans = 0 ;
-        for (int i = 0 ; i <= m ; ++ i ) {
-            ans += ( ( ( m - i ) & 1 ) == 1 ? -1 : 1 ) * power ( (T)i , n ) * invfac ( i ) * invfac ( m - i ) ; }
-        return ans ;
+    T Stirling(int n, int m) {
+        T ans = 0;
+        for (int i = 0; i <= m; ++i) {
+            ans += (((m - i) & 1) == 1 ? -1 : 1) * power((T) i, n) * invfac(i) * invfac(m - i);
+        }
+        return ans;
     }
-    T Catalan (int n) {
-        return binom ( 2 * n , n ) * inv ( n + 1 ) ;
+
+    T Catalan(int n) {
+        return binom(2 * n, n) * inv(n + 1);
     }
+
 /**
  * 算法：卢卡斯定理
  * 作用：大数组合数
@@ -63,9 +72,10 @@ struct Comb {
  * p为Z的质数
  * 时间复杂度为O(logp)
 */
-    T lucas ( i64 n , i64 m ) {
-        if ( m == 0 ) return T(1);
-        return binom ( n % T::getMod () , m % T::getMod () ) * lucas ( n / T::getMod () , m / T::getMod () ) ;
+    T lucas(i64 n, i64 m) {
+        if (m == 0) return T(1);
+        return binom(n % T::getMod(), m % T::getMod()) * lucas(n / T::getMod(), m / T::getMod());
     }
-} ;
-Comb <Z> comb ;
+};
+
+Comb<Z> comb;
--- 杂/手写/取模类.cpp
+++ 杂/手写/取模类.cpp
@@ -13,14 +13,15 @@ template<int P>
 struct mod_int {
     int x;
     constexpr static int mod{0};
-    constexpr mod_int ():x{}{}
-    constexpr mod_int (long long x):x(norm(x % getMod())){}
-    
+    constexpr mod_int() : x{} {}
+    constexpr mod_int(long long x) : x(norm(x % getMod())) {}
+
     constexpr int norm(int x) {
         if (x >= P) x -= P;
         if (x < 0) x += P;
         return x;
     }
+
     constexpr static void setMod(int x) {
         mod = x;
     }
@@ -30,50 +31,56 @@ struct mod_int {
     constexpr mod_int operator-() {
         return -x;
     }
-    constexpr mod_int& operator+= (mod_int rhs) {
+
+    constexpr mod_int &operator+=(mod_int rhs) {
         x = norm(x + rhs.x);
         return *this;
     }
-    constexpr mod_int& operator-= (mod_int rhs) {
+    constexpr mod_int &operator-=(mod_int rhs) {
         x = norm(x - rhs.x);
         return *this;
     }
-    constexpr mod_int& operator*= (mod_int rhs) {
+    constexpr mod_int &operator*=(mod_int rhs) {
         x = 1ll * x * rhs.x % getMod();
         return *this;
     }
-    constexpr mod_int inv () {
+
+    constexpr mod_int inv() {
         return power(*this, P - 2);
     }
-    constexpr mod_int& operator/= (mod_int rhs) {
+    constexpr mod_int &operator/=(mod_int rhs) {
         x = 1ll * x * rhs.inv().x % getMod();
         return *this;
     }
-    constexpr friend mod_int operator+ (mod_int lhs, mod_int rhs) {
+
+    constexpr friend mod_int operator+(mod_int lhs, mod_int rhs) {
         return lhs += rhs;
     }
-    constexpr friend mod_int operator- (mod_int lhs, mod_int rhs) {
+    constexpr friend mod_int operator-(mod_int lhs, mod_int rhs) {
         return lhs -= rhs;
     }
-    constexpr friend mod_int operator* (mod_int lhs, mod_int rhs) {
+    constexpr friend mod_int operator*(mod_int lhs, mod_int rhs) {
         return lhs *= rhs;
     }
-    constexpr friend mod_int operator/ (mod_int lhs, mod_int rhs) {
+    constexpr friend mod_int operator/(mod_int lhs, mod_int rhs) {
         return lhs /= rhs;
     }
-    constexpr friend bool operator== (mod_int lhs, mod_int rhs) {
+    constexpr friend bool operator==(mod_int lhs, mod_int rhs) {
         return lhs.x == rhs.x;
     }
-    constexpr friend bool operator!= (mod_int lhs, mod_int rhs) {
+    constexpr friend bool operator!=(mod_int lhs, mod_int rhs) {
         return lhs.x != rhs.x;
     }
-    constexpr friend istream& operator>> (istream& flow, mod_int& rhs) {
+
+    template<class istream>
+    constexpr friend istream &operator>>(istream &flow, mod_int &rhs) {
         long long x;
         flow >> x;
         rhs = x;
         return flow;
     }
-    constexpr friend ostream& operator<< (ostream& flow, mod_int rhs) {
+    template<class ostream>
+    constexpr friend ostream &operator<<(ostream &flow, mod_int rhs) {
         return flow << rhs.x;
     }
 };
@@ -81,5 +88,5 @@ struct mod_int {
 template<int P, int x>
 constexpr mod_int<P> invx = mod_int<P>(x).inv();
 
-constexpr int P = 1e9 + 7;
+constexpr int P = 998244353;
 using Z = mod_int<P>;
