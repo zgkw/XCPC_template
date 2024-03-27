97b668e33e48c1b14b3283bc05bb2ba10c169545
Kaiwen Zhang <2695415649@qq.com>
2024-03-27 17:25:55 +0800

    格式化排版

--- 数据结构/树状数组.cpp
+++ 数据结构/树状数组.cpp
@@ -1,24 +1,24 @@
-template <typename T>
+template<typename T>
 struct Fenwick {
     int n;
-    std::vector<T> a;
-    
+    std::vector <T> a;
+
     Fenwick(int n_ = 0) {
         init(n_);
     }
-    
+
     void init(int n_) {
         n = n_;
         a.assign(n + 1, T{});
     }
-    
+
     void add(int x, const T &v) {
         if (x <= 0 || x > n) return;
         for (int i = x; i <= n; i += i & -i) {
             a[i] = a[i] + v;
         }
     }
-    
+
     T Query(int x) {
         if (x <= 0) return T{};
         if (x > n) x = n;
@@ -28,12 +28,12 @@ struct Fenwick {
         }
         return ans;
     }
-    
+
     T range_Query(int l, int r) {
-        if ( l > r ) return 0 ;
+        if (l > r) return 0;
         return Query(r) - Query(l - 1);
     }
-    
+
     int kth(const T &k) {
         int x = 0;
         T cur{};
