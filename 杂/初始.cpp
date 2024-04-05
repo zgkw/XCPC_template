00577fd7b2e97ba0a2318a24a3421653487c3a55
Kaiwen Zhang <2695415649@qq.com>
2024-04-05 19:16:26 +0800

    Update 初始.cpp

--- 杂/初始.cpp
+++ 杂/初始.cpp
@@ -1,57 +1,35 @@
-# include  <bits/stdc++.h>
+# include <bits/stdc++.h>
+using namespace std;
 # ifdef LOCAL
-    # include "C:\Users\Kevin\Desktop\demo\C++_code\debug.h"
-#endif
-using  namespace  std ;
+    # include "C:\Users\Kevin\Desktop\demo\save\debug.h"
+# else
+# define debug(...) 114514
+# define ps 114514
+# endif
 
-namespace predefine {
-    const long long inf = 0x3f3f3f3f3f3f3f3f ;
-    using ll = long long ;using i64 = long long ;using Long = long long ;
-    using Int = __int128 ;using pll = pair < Long , Long > ;
+using ll = long long;
+using i64 = long long;
 
-    # define af( i , a , b ) for ( Long i = a ; i <= b ; ++ i )
-    # define df( i , a , b ) for ( Long i = a ; i >= b ; -- i )
-
-    # define endl '\n'
+void solve() {
+    
 }
-using namespace predefine ;
 
-//-----  板子  ---------------------------------------------------------------------------------------------------------
-
-//-----  板子  ---------------------------------------------------------------------------------------------------------
-
-inline Long sum1 ( Long _a ) { return ( _a * ( _a + 1 ) ) >> 1 ; }
-inline Long sum2 ( Long _b ) { return _b * ( _b + 1 ) * ( 2 * _b + 1 ) / 6  ; }
-
-Long dx [ 4 ] = { -1 , 0 , 1 , 0 } ;
-Long dy [ 4 ] = { 0 , 1 , 0 , -1 } ;
-
-void solve () {
-}
-/*
-*/
 int  main () {
-# ifndef cin or cout
+# ifndef cin 
     std::ios::sync_with_stdio (false);
     std::cin.tie (nullptr) ;
 # endif
+    // __fin("C:\\Users\\Kevin\\Desktop\\cpp\\in.in") ;
 
-//	__fin ( "in.in" ) ;
-	ll _ = 1 ;
-	// rd ( _ ) ;
- 	// cin >> _ ;
-	while ( _ -- ) {
-        // Dgop ;
-        solve () ;
-        // Dged ;
-	}
-	return 0 ;
+    i64 _ = 1 ;
+    // cin >> _ ;
+    while (_ --) {
+        solve ();
+# ifdef LOCAL
+# ifndef cin
+    cout.flush();
+# endif
+# endif
+    }
+    return 0 ;
 }
-
-/*
-//-----  数据  ---------------------------------------------------------------------------------------------------------
-
-----------
-*/
-
-
