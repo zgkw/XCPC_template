acd230c7b6cff2ce7a3658fcb5e4a41825d4456c
Kaiwen Zhang <2695415649@qq.com>
2024-04-20 11:08:37 +0800

    update trie

--- 字符串/trie.cpp
+++ 字符串/trie.cpp
@@ -1,29 +1,24 @@
-struct Trie {
-    struct Node {
-        ll sum = 0 ;
-        array <Node*, 27> ch ;
-        Node () {ch.fill ( nullptr );} ; 
-    };
-    Node* root ;
+constexpr int N = 2e5;
 
-    Trie ( bool have_default ) {
-        root = new Node () ;
-    }
-
-    void add ( const vector < int >& v ) {
-        auto p = root ;
-        for ( auto u : v ) {
-            if ( not p->ch [ u ] ) { p -> ch [ u ] = new Node () ;}
-            p = p ->ch [ u ] ;
-            ++ p -> sum ;
-        }
-    }
-
-    void add ( string s , char off = 'a' ) {
-        vector < int > v ;
-        for ( auto u : s ) {
-            v.push_back ( u - off ) ;
-        }
-        v.push_back ( 27 ) ;
-    }
+struct node{
+    int _ch[2];
+    int R = -1, L = -1;
+    node *ch(int x) const;
+    void _new(int x);
+    bool is_null();
+    bool not_null();
 };
+node B[N * 30];
+int idx = 1;
+node *node::ch(int x) const {
+    return B + _ch[x];
+}
+void node::_new(int x) {
+    _ch[x] = ++ idx;
+}
+bool node::is_null() {
+    return this == B;
+}
+bool node::not_null() {
+    return this != B;
+}
--- 杂/对拍/data.cpp
+++ 杂/对拍/data.cpp
@@ -1,1 +1,19 @@
+# include <bits/stdc++.h>
+using namespace std;
 
+using ll = long long;
+using i64 = long long;
+
+int  main () {
+# ifndef cin 
+    std::ios::sync_with_stdio (false);
+    std::cin.tie (nullptr) ;
+# endif
+    std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+    uniform_int_distribution<long long> rnd(1, 1000000000000000000);
+    auto rnd = [&] {
+    	return rnd(rng);
+    };
+
+	return 0 ;
+}
